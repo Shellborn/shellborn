18df55b612701dcb85bcb3243b7d19985383bb49
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-11-21 11:32:18 +0100

    added cli structure and tests

--- shellborn-cli/src/main.rs
+++ shellborn-cli/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    shellborn::say_hello()
+use std::fs;
+use std::path::PathBuf;
+use anyhow::Context;
+use clap::Parser;
+use clap_verbosity_flag::Verbosity;
+use log::{debug, error, info};
+
+#[derive(Parser)]
+#[command(author, version)]
+struct Args {
+    file_path: PathBuf,
+    #[command(flatten)]
+    verbose: Verbosity,
+}
+
+pub fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    env_logger::Builder::new()
+        .filter_level(args.verbose.log_level_filter())
+        .init();
+
+    debug!("start reading file");
+    let file_content = fs::read_to_string(&args.file_path);
+    if file_content.is_err() { error!("failed to read file content")  }
+    let file_content = file_content.with_context(|| format!("could not read file '{}'", args.file_path.display()))?;
+    info!("file content was read");
+
+    // TODO: Use Shellborn
+    Ok(())
 }
